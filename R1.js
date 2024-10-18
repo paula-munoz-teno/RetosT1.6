1fa3e96ea10a251f9455f449d456dda55a35d2df
Paula-Munoz-Teno <paulamtlaboral@gmail.com>
2024-10-18 10:26:18 +0200

    Subida de R1

---
+++ R1.js
@@ -0,0 +1,45 @@
+
+
+function suma (x,y){
+    let resultado = x+y;
+    console.log(resultado);    
+    return resultado;
+ 
+ }
+ 
+ function resta(x,y){
+    let resultado = x-y;
+    console.log(resultado);    
+    return resultado;
+ 
+ }
+ 
+ 
+ function producto(x,y){
+    let resultado = x*y;
+    console.log(resultado);    
+    return resultado;
+ 
+ }
+ 
+ 
+ function division(x,y){
+    let resultado = x/y;
+    console.log(resultado);    
+    return resultado;
+ 
+ }
+ 
+ 
+ function cuadradoDeN(x){
+    let resultado = x*x;
+    console.log(resultado);    
+    return resultado;
+ 
+ }
+ 
+ //Comprobación
+ //Sale doble, habría que quitar el console.log de dentro
+ let resulcuadradoDeN = cuadradoDeN(4);
+ console.log(resulcuadradoDeN);
+ 
